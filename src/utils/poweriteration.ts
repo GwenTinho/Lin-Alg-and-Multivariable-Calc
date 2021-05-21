df045f3fcdad1c1bf17285d1fec16dc7d2be4088
Gwentinho <qschroed@gmail.com>
2021-05-21 20:53:08 +0200

    moved poweriteration to ts

---
+++ src/utils/poweriteration.ts
@@ -0,0 +1,31 @@
+import { Complex } from "../Complex";
+import { Matrix } from "../Matrix";
+import { Vector } from "../Vector";
+
+const iterations = 50;
+
+/**
+ * https://en.wikipedia.org/wiki/Power_iteration
+ * @param {Matrix} matrix
+ * @param {Number[]} eigenValues
+ * @returns
+ */
+export function powerIteration(matrix: Matrix, eigenValues: Complex[] = [], depth = 0): Complex[] {
+    const n = matrix.dimensions()[0];
+    let b_k = Vector.randomVector(n);
+
+    for (let i = 0; i < iterations; i++) {
+        let prod_A_b_k = matrix.multByVector(b_k);
+        b_k = prod_A_b_k.divReal(prod_A_b_k.norm());
+    }
+
+
+    let eigenV = matrix.multByVector(b_k).findMultiplier(b_k).add(eigenValues.reduce((curr, acc) => acc = acc.add(curr), Complex.ZERO));
+    eigenValues.push(eigenV);
+
+    if (depth === n - 1) return eigenValues;
+
+    depth++;
+
+    return powerIteration(matrix.sub(Matrix.getIdentityMatrixMultiple(n, eigenV)), eigenValues, depth);
+}
