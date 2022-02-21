abb48f1f215e68dfeb8835a25e1245753c0182d7
gwentinho <qschroed@gmail.com>
2022-02-21 11:24:06 +0100

    some basic ss stuff

---
+++ stateSpaceSystems/StateSpaceSystem.js
@@ -0,0 +1,40 @@
+import fadeev from "../linalg/fadeev";
+import Matrix from "../linalg/Matrix";
+import Polynomial from "../linalg/Polynomial";
+import Vector from "../linalg/Vector";
+
+class StateSpaceSystem{
+
+    /**
+     *
+     * @param {Matrix} A
+     * @param {Matrix} B
+     * @param {Matrix} C
+     * @param {Matrix} D
+     */
+    constructor(A,B,C,D){
+        this.A = A;
+        this.B = B;
+        this.C = C;
+        this.D = D;
+
+        // dimensionality checks are missing
+    }
+
+    transferFunctionAt(s){
+        const {chracteristicPolyterms, auxMatricies} = fadeev.fadeev(this.A);
+
+        const polyAt = (new Polynomial(new Vector(chracteristicPolyterms))).eval(s);
+
+        const n = this.A.getDimensions()[0];
+
+        let inverseTerm = Matrix.getIdentityMatrixMultiple(n,0);
+
+        for (let i = 0; i < n; i++) {
+            inverseTerm = inverseTerm.add(auxMatricies[i].multByReal(s ** (n-i)));
+        }
+        inverseTerm = inverseTerm.multByReal(1/polyAt);
+
+        return this.C.mul(inverseTerm).mul(this.B).add(this.D);
+    }
+}
