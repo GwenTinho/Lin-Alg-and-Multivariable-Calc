60a98726a9c9d1a252ba097520d41baaad3f1818
gwentinho <qschroed@gmail.com>
2022-04-04 18:30:11 +0200

    more integrals

--- integrator/index.js
+++ integrator/index.js
@@ -1,6 +1,6 @@
 // include a exclude b
 
-function approxIntegral(a,b, f){
+function approxIntegralOneVar(a,b, f){
     let estimate = 0;
     const n = 1e6;
 
@@ -9,4 +9,15 @@ function approxIntegral(a,b, f){
     }
 
     return (b-a) * estimate / n;
+}
+
+function approxIntegral(ranges, f){
+    let estimate = 0;
+    const n = 1e6;
+
+    for (let i = 0; i < n; i++) {
+        estimate += f(ranges.map(range => Math.random() * (range[1]-range[0]) + range[0]));
+    }
+
+    return ranges.reduce((acc, curr) => acc * (curr[1]-curr[0]), 1) * estimate / n;
 }
